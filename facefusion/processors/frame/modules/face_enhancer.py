500769fa351598a2d945a8505a1ed4dc379b877f
Lucas Hansen <veered@users.noreply.huggingface.co>
2024-02-06 19:29:19 +0000

    Update facefusion/processors/frame/modules/face_enhancer.py

--- facefusion/processors/frame/modules/face_enhancer.py
+++ facefusion/processors/frame/modules/face_enhancer.py
@@ -82,7 +82,7 @@ def get_frame_processor() -> Any:
 	with THREAD_LOCK:
 		if FRAME_PROCESSOR is None:
 			model_path = get_options('model').get('path')
-            FRAME_PROCESSOR = onnxruntime.InferenceSession(model_path, providers = [ 'CPUExecutionProvider' ])
+			FRAME_PROCESSOR = onnxruntime.InferenceSession(model_path, providers = [ 'CPUExecutionProvider' ])
 			# FRAME_PROCESSOR = onnxruntime.InferenceSession(model_path, providers = facefusion.globals.execution_providers)
 	return FRAME_PROCESSOR
 
