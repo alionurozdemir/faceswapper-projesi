6a4bb5e39d5e15185dee1e1c76ec018f3f6a3931
Lucas Hansen <veered@users.noreply.huggingface.co>
2024-02-06 03:25:17 +0000

    Update facefusion/core.py

--- facefusion/core.py
+++ facefusion/core.py
@@ -26,7 +26,7 @@ warnings.filterwarnings('ignore', category = UserWarning, module = 'torchvision'
 
 
 def cli() -> None:
-	signal.signal(signal.SIGINT, lambda signal_number, frame: destroy())
+	# signal.signal(signal.SIGINT, lambda signal_number, frame: destroy())
 	program = ArgumentParser(formatter_class = lambda prog: HelpFormatter(prog, max_help_position = 120), add_help = False)
 	# general
 	program.add_argument('-s', '--source', help = wording.get('source_help'), dest = 'source_path')
